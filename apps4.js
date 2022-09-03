b6e0beb4597714d3dedfa445fb4532e664e48714
nicavasquez <94380607+nicavasquez@users.noreply.github.com>
2022-09-03 11:38:21 -0300

    Agrego uso de arrays en forma de carro de compras. Funcion para calcular el total de la compra.

--- apps4.js
+++ apps4.js
@@ -1,6 +1,8 @@
 
 let nombre = ""
+const carrito = [];
 
+// Bienvenida - funciones solicitarNombre y saludar
 solicitarNombre(nombre);
 
 function solicitarNombre(nombre) {
@@ -12,22 +14,24 @@ function saludar(nombre) {
     console.log(`Bienvenido ${nombre}, juntos vamos a seleccionar tu pedido.`);
 }
 
+// clase de productos con metodo de valorFinal (ver si ValorFinal lo dejo de usar)
 class Producto {
     constructor (nombre, precio, descripcion, cantidad){
         this.nombre = nombre;
         this.precio = precio;
         this.descripcion = descripcion;
-        this.cantidad = 0;
+        // this.cantidad = 0;
 
     }
 
 
-    ValorFinal(){
-        return this.precio * this.cantidad;
-    }
+    // ValorFinal(){
+    //     return this.precio * this.cantidad;
+    // }
 
 }
 
+// creo los distintos productos (puedo crear listas que contengan distintas clases de pizzas, hamburguesas , lomitos, etc)
 const producto1 = new Producto ("Pizza", 1200, "Pizza especial");
 const producto2 = new Producto ("Hamburguesa", 800, "Hamburguesa simple con fritas");
 const producto3 = new Producto ("Lomito", 1500, "Lomo especial con fritas");
@@ -39,71 +43,102 @@ console.log(`2) ${producto2.nombre}`);
 console.log(`3) ${producto3.nombre}`);
 console.log(`4) ${producto4.nombre}`);
 
-let option = prompt(`Elija una opcion:
-1: Pizza
-2: Hamburguesa
-3: Lomito
-4: Sand. Milanesa
-5: No quiero nada.`);
 
+// funcion para escoger opciones, cantidad, agregar productos dentro de carrito y calculo de valor total 
 seleccionarOpcion();
 
 function seleccionarOpcion () {
+
+    let option = prompt(`Elija una opcion:
+    1: Pizza
+    2: Hamburguesa
+    3: Lomito
+    4: Sand. Milanesa
+    5: No quiero nada.`);
+    switch (option) {
+        case "1":
+            console.log("Has seleccionado Pizza");
+            carrito.push(producto1);
+            cantidad();
+            agregarProducto();  
+            break;
+        case "2":
+            console.log("Has seleccionado Hamburguesa");
+            carrito.push(producto2);
+            cantidad();
+            agregarProducto();      
+            break;
+        case "3":
+            console.log("Has seleccionado Lomito");
+            carrito.push(producto3);
+            cantidad();
+            agregarProducto();        
+            break;
+        case "4":
+            console.log("Has seleccionado Sand. Milanesa");
+            carrito.push(producto4)
+            cantidad();
+            agregarProducto();       
+            break;
+        case "5":
+            console.log("Has seleccionado No quiero nada.");        
+            break;    
+
+        default:
+            console.log("No has iniciado una opción válida.");
+            alert("No has iniciado una opción válida."); 
+            alert("Selecciona una opción válida.");
+            seleccionarOpcion();
+            break;
+}
+}
+
+// funcion para agregar productos, dentro de la funcion seleccionarOpcion()
+function agregarProducto() {
+    console.log("Si quieres agregar otro producto presiona 1, en caso contrario presiona 2.");
+    valor = prompt(`Si quieres agregar otro producto presiona 1, en caso contrario presiona 2. Elija:
+    1: Si
+    2: No`);
+
+    switch (valor) {
+        case "1":
+            console.log("Has seleccionado Si");
+            seleccionarOpcion();
+            
+
+            break;
+        case "2":
+            console.log("Has seleccionado No");        
+            // console.log(carrito)
+            break;
+            
+        default:
+            console.log("No has iniciado una opción válida.");
+            alert("No has iniciado una opción válida."); 
+            alert("Selecciona una opción válida.");
+            agregarProducto();    
+}
+}
+
+// funcion cantidad, para especificar la cantidad de un producto, funciona dentro de la funcion seleccionarOpcion()
+function cantidad() {
     
-switch (option) {
-    case "1":
-        console.log("Has seleccionado Pizza");     
-        break;
-    case "2":
-        console.log("Has seleccionado Hamburguesa");        
-        break;
-    case "3":
-        console.log("Has seleccionado Lomito");        
-        break;
-    case "4":
-        console.log("Has seleccionado Sand. Milanesa");        
-        break;
-    case "5":
-        console.log("Has seleccionado No quiero nada.");        
-        break;    
-
-    default:
-        console.log("No has iniciado una opción válida.");
-        option = prompt(`Elija otra opcion:
-1: Pizza
-2: Hamburguesa
-3: Lomito
-4: Sand. Milanesa
-5: No quiero nada.`); 
-        seleccionarOpcion();
-        break;
+    cant = prompt("Dime que cantidad quieres de este producto: ");
+    console.log(`Has seleccionado ${cant} unidades.`);
+    carrito.push(cant);
+    
 }
+// funcion para calculo del monto total a pagar, a partir de la lista creada de productos.
+let montoApagar = 0
+function montoTotal (){
+    // console.log(carrito);
+    for (let index = 0; index < carrito.length; index=index+2) {
+        const element = carrito[index];
+        let resultado = element.precio * carrito[index+1];
+        montoApagar = montoApagar + resultado;    
+    }
+console.log(`El monto a pagar es ${montoApagar}`);    
 }
-
-console.log(`Esta es la opción ${option}`);
+montoTotal();
 
 
-
-if (option == "1") {
-    producto1.cantidad = Number(prompt("Dime cuantas unidades quieres: "));
-    console.log(`Has pedido ${producto1.cantidad} de ${producto1.nombre} que tiene un precio de ${producto1.precio}.`);
-    console.log(`Entonces debes pagar ${producto1.ValorFinal()}`);
-}
-else if (option == "2") {
-    producto2.cantidad = Number(prompt("Dime cuantas unidades quieres: "));
-    console.log(`Has pedido ${producto2.cantidad} de ${producto2.nombre} que tiene un precio de ${producto2.precio}.`);
-    console.log(`Entonces debes pagar ${producto2.ValorFinal()}`);
-}
-else if (option == "3") {
-    producto3.cantidad = Number(prompt("Dime cuantas unidades quieres: "));
-    console.log(`Has pedido ${producto3.cantidad} de ${producto3.nombre} que tiene un precio de ${producto3.precio}.`);
-    console.log(`Entonces debes pagar ${producto3.ValorFinal()}`);
-}
-else if (option == "4") {
-    producto4.cantidad = Number(prompt("Dime cuantas unidades quieres: "));
-    console.log(`Has pedido ${producto4.cantidad} de ${producto4.nombre} que tiene un precio de ${producto4.precio}.`);
-    console.log(`Entonces debes pagar ${producto4.ValorFinal()}`);
-}
-else {
-    console.log("No tienes que pagar nada.")
-}
